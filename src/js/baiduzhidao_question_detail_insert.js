4f50728c1cd82d82d28bee0d70f68835a41a1b7b
tansuo1989 <huangui9@qq.com>
2020-05-06 09:57:25 +0800

    把no_name 改成 匿名

--- src/js/baiduzhidao_question_detail_insert.js
+++ src/js/baiduzhidao_question_detail_insert.js
@@ -5,7 +5,7 @@
     var page=window.F.context("page");
     var url="https://zhidao.baidu.com/usercenter?uid="+page.encodeUid;
     var date=fun.date_format(page.createTime);
-    var xm=page.userName?page.userName:"no_name";
+    var xm=page.userName?page.userName:"匿名";
     var tags=page.tags;
     // console.log("tags",tags);
     var html="<span><a href='"+url+"' target='_blank'>@"+xm+": "+date+"</a>  <br/> <span>标签: "+tags+"</span></span>";

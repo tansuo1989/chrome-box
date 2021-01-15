761033a854c91ffa3281dca1b98fbf048635caef
tansuo19 <huangui9@qq.com>
2021-01-15 16:49:24 +0800

    补充微博新的自动跳转外链

--- src/js_content/sina.js
+++ src/js_content/sina.js
@@ -1,9 +1,11 @@
 
 
- var desc=$("p.desc").text();
- console.log("desc",desc)
- if(desc=="如需浏览，请长按网址复制后使用浏览器访问"){
+ if($("p.desc").text()=="如需浏览，请长按网址复制后使用浏览器访问"){
      var link=$("p.link").text();
-     $("p.link").after("<p style='text-align:center;'><a href='"+link+"'>自动跳转中...</a></p>");
-     location.href=link;
+ }
+ else if($(".open-url a").text()=='继续访问'){
+     var link=$(".open-url a").attr("href");
+ }
+ if(link){
+    location.href=link;
  }
